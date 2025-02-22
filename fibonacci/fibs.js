4c85002fc4e400aae78104502176bd37f5617b49
Tupay Felkin <tupai@live.co.uk>
2025-02-22 14:16:52 +0000

    FEAT: Fibonacci iterative approach

---
+++ fibonacci/fibs.js
@@ -0,0 +1,27 @@
+// Iterative approach:
+
+/**
+ * 
+ * @param {*} num the number of elements in the fibonacci sequence
+ * @returns the array of elements in the fibonacci sequence
+ */
+
+const fibs = (num) => {
+    let answer = [];
+    let x= 0; //1st element
+    let y = 0; //2nd element
+    let z; //sum of x,y
+    let i = 2
+
+    answer.push(x);
+    answer.push(y)
+
+    while (i < num) {
+        z = x + y;
+        x = y;
+        y = z;
+        answer.push(z) //store current element
+        i++
+    }
+    return answer
+}
